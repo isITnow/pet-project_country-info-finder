a5c8497248b4a9f2b4c49e04a2814b4c2ff772ef
Roman Serediuk <box4rom@gmail.com>
2022-11-04 15:21:00 +0200

    add CustomSelect component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Controls } from 'components/Controls';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
 
 export const App = () => {
-  return (
-    <>
-      <Header />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Main>
+                <Controls />
+            </Main>
+        </>
+    );
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,36 +3,43 @@ import { IoSearch } from 'react-icons/io5';
 
 const InputContainer = styled.label`
     background-color: var(--colors-ui-base);
-    padding: 1rem 2rem;
+    padding: 1rem 1rem;
     display: flex;
     align-items: center;
 
     border-radius: var(--radii);
     box-shadow: var(--shadow);
     width: 100%;
-    margin-bottom: 1.5rem;
+    margin-bottom: 1rem;
 
     @media (min-width: 767px) {
-        width: 280px;
+        padding: 1rem 2rem;
+
         margin-bottom: 0;
+        width: 280px;
     }
 `;
 const Input = styled.input.attrs({
     type: 'search',
     placeholder: 'Search for a country...',
 })`
-    margin-left: 2rem;
+    margin-left: 1rem;
     border: none;
     outline: none;
-    background-color: var(--colors-bg);
-    color: var(--colors-text);
+    color: var(--color-text);
+    background-color: var(--colors-ui-base);
 `;
 
 export const Search = ({ search, setSearch }) => {
     return (
-        <InputContainer>
-            <IoSearch />
-            <Input value={search} onChange={e => setSearch(e.target.value)} />
-        </InputContainer>
+        <>
+            <InputContainer>
+                <IoSearch />
+                <Input
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
+            </InputContainer>
+        </>
     );
 };
