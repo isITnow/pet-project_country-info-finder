bca6f749bace65151dc43fdee01ac94c2f1064df
Roman Serediuk <box4rom@gmail.com>
2022-11-05 10:48:49 +0200

    update basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pet-project_country-info-finder/">
         <App />
     </BrowserRouter>
     // </React.StrictMode>
